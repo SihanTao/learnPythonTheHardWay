d40fdb150e0f6273006703a92eea84dea61c5291
st2220 <sihan.tao20@imperial.ac.uk>
2021-11-21 05:56:41 -0800

    functions.py

---
+++ ex18/functions.py
@@ -0,0 +1,11 @@
+def print_two(*args):
+    arg1, arg2 = args
+    print(f"arg1: {arg1}, arg2:{arg2}")
+
+
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2:{arg2}")
+
+
+print_two("Tao", "Sihan")
+print_two_again("Liu", "tony")
