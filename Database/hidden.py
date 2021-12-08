8800d2ffd87246765d63a5b6788666ea980809c4
st2220 <sihan.tao20@imperial.ac.uk>
2021-12-08 08:13:48 -0800

    database

---
+++ Database/hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "AYdFBGuG40h8HZexC4Fc4Xpcs",
+            "consumer_secret": "k7ESxP2Zi95SSwNz7g5xh3phvgtEWpRY2c4sHZEaQZTucLPFGf",
+            "token_key": "1468318581255507974-aVPq52cmFSQGsALoGs575sxKay5zjn",
+            "token_secret": "6WgqNEjjXvNvNfVFckyBEqcTVahhqqJa4LYFjTLvH5Vz2"}
